2bfd375769e6ea230ae9ca002c96047926d9cbc3
ltchelishvili1 <106867681+ltchelishvili1@users.noreply.github.com>
2022-06-04 15:20:22 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () => {
     <>
     <Navbar/>
     <Routes>
-      <Route path='/facebook.com' element={<Coins coins={coins} />} />
+      <Route path='/' element={<Coins coins={coins} />} />
       <Route path='/coin' element={<Coin />}>
       <Route path=':coinId' element={<Coin/>}/>
       </Route>
